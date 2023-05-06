2f0a1d64020df4674157f36f6a4861b009a29ed5
Architect <hola+git@kike.wtf>
2023-05-06 05:53:19 +0200

    🐛 Git config is required

--- scripts/fake-git.sh
+++ scripts/fake-git.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 BASEDIR=$(dirname "$0")/..
 git -C "${BASEDIR}/public/" init
+git -C "${BASEDIR}/public/" config user.name "SupaDupaPumaDev"
+git -C "${BASEDIR}/public/" config user.email "hola+jokes@kike.wtf"
 echo "You're a hecker if you found this, but you're also a bad person if you're reading it intentionally." > "${BASEDIR}/public/secret.txt"
 echo "-K" >> "${BASEDIR}/public/secret.txt"
 git -C "${BASEDIR}/public/" add secret.txt
